646626a08a39d5e01f3d247592f0fd9b76b38c44
Riccardo Papini <rickypapini@gmail.com>
2024-05-26 22:16:48 +0100

    Edit vercel cron job values

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,14 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import emailjs from "@emailjs/nodejs";
 
+async function streamToString(stream: any) {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+}
+
 export async function POST(req: NextRequest) {
-  const msg = await req.json();
+  // const msg = await req.json();
+  const data = streamToString(req.body);
 
   const { EMAILJS_PUBLIC_KEY, EMAILJS_PRIVATE_KEY } = process.env;
 
   const templateParams = {
     to_name: "Ricky",
-    message: msg,
+    message: data,
   };
 
   emailjs
